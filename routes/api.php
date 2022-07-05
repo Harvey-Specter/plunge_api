f78657453bbf023f57e4b20b19fecb3c9e0776de
vagrant <vagrant@homestead>
2022-07-05 02:06:20 +0000

    用户注册

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthorizationsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// 第三方登录
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('socials/{social_type}/authorizations', [AuthorizationsController::class, 'socialStore'])
+                    ->where('social_type', 'wechat')
+                    ->name('socials.authorizations.store');
+
+// 登录
+Route::post('authorizations', [AuthorizationsController::class, 'store'])
+    ->name('authorizations.store');
+// 用户注册
+Route::post('users', [UsersController::class, 'store'])
+->name('users.store');
+
+// 刷新token
+Route::put('authorizations/current', [AuthorizationsController::class, 'update'])
+    ->name('authorizations.update');
+// 删除token
+Route::delete('authorizations/current', [AuthorizationsController::class, 'destroy'])
+    ->name('authorizations.destroy');
