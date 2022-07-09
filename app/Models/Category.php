0a55cd21c01d86aa505f50643cf7bfdc085ab9fd
Harvey <lihong671@gmail.com>
2022-07-10 07:45:10 +0800

    cate belong user

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
-    
     protected $fillable = ['name', 'code', 'remark', 'owner', 'stock_count', 'create_time'];
+    
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
