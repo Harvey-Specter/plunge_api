81c950264f582a1126455801ec917c2ea67f513e
Harvey <lihong671@gmail.com>
2022-07-10 08:54:17 +0800

    排序

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,31 @@ class Category extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->name();
+                break;
+        }
+    }
+
+    public function scopeName($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 name 时间戳的更新
+        return $query->orderBy('name', 'Asc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
 }
